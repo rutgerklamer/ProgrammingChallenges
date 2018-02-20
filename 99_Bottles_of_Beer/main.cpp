17464c7fc66fc07c88a88b6b4e52a8701cb342ab
Rutger Klamer <rutger@rutgerklamer.nl>
2018-02-20 15:12:39 +0100

    Add 99 Bottles of Beer Challenge

---
+++ 99_Bottles_of_Beer/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+	for (int i = 99; i > 0; i--) 
+		std::cout << i << " Bottles of beer on the wall,\n" << i << " Bottles of beer.\nTake one down, pass it around,\n";
+	return 0;
+}
