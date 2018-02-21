239497ae87e6eae533d823d575d9248d70122f70
Rutger Klamer <rutger@rutgerklamer.nl>
2018-02-21 10:01:20 +0100

    Update last line

--- 99_Bottles_of_Beer/main.cpp
+++ 99_Bottles_of_Beer/main.cpp
@@ -3,6 +3,8 @@
 int main()
 {
 	for (int i = 99; i > 0; i--) 
-		std::cout << i << " Bottles of beer on the wall,\n" << i << " Bottles of beer.\nTake one down, pass it around,\n";
+		std::cout << i << " Bottles of beer on the wall, " << i << " Bottles of beer.\nTake one down and pass it around, " << i-1 << " Bottles of beer on the wall\n\n";
+
+	std::cout <<  "No more Bottles of beer on the wall, " <<  "No more Bottles of beer.\nGo to the store and buy some more, " << "99 Bottles of beer on the wall\n\n";
 	return 0;
 }
