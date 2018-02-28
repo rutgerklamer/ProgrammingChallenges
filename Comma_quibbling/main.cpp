6f57ddb28187cf705bf8903e232c7c007dbaf1f2
Rutger Klamer <rutger@rutgerklamer.nl>
2018-02-28 11:14:10 +0100

    Add { and } symbols

--- Comma_quibbling/main.cpp
+++ Comma_quibbling/main.cpp
@@ -5,10 +5,12 @@
 int main() {
 	std::vector<std::string> strings = { "ABC", "DEF", "G", "H" };
 	for (int i = 0; i < strings.size(); i++) {
+		std::cout << "{ ";
 		for (int j = 0; j < i+1; j++) {
 			std::cout << (j == 0 ? "" : (j < i ? ", " : " and ")) << strings[j] << std::flush;
 		}
-		std::cout << std::endl;
+		std::cout << " }\n";
 	}
 	return 0;
 }
+
