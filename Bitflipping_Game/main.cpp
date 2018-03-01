0ab548fa56e4d2ee0c1013c025507f6bf51f2d00
Rutger Klamer <rutger@rutgerklamer.nl>
2018-03-01 14:50:25 +0100

    Fix compile error, fix not able to finish

--- Bitflipping_Game/main.cpp
+++ Bitflipping_Game/main.cpp
@@ -1,10 +1,15 @@
 #include <iostream>
 
+int flipRowColumn(char p, int s);
+void showTable(int n);
+
 int main() {
 	int starting = rand() % 511;
-	int target = rand() % 511;
-	while (target == starting)
-		target = rand() % 511;
+	int target = starting;
+	char ip[] = { '0', '1', '2', 'a', 'b', 'c' };
+	while (target == starting) 
+		for (int i = 0; i < rand() % 5 + 5; i++)
+			target = flipRowColumn(ip[rand()%6], target);
 
 	std::cout << "STARTING: " << std::endl;
 	showTable(starting);
@@ -16,33 +21,32 @@ int main() {
 		char p = ' ';
 		while ((p > '2' || p < '0') && (p > 'c' || p < 'a'))
 			std::cin >> p;
-
-		switch (p) {
-			case '0':
-				starting ^= (1 << (8)) + (1 << (7)) + (1 << (6));
-				break;
-			case '1':
-				starting ^= (1 << (5)) + (1 << (4)) + (1 << (3));
-				break;
-			case '2':
-				starting ^= (1 << (2)) + (1 << (1)) + (1 << (0));
-				break;
-			case 'a':
-				starting ^= (1 << (2)) + (1 << (5)) + (1 << (8));
-				break;
-			case 'b':
-				starting ^= (1 << (1)) + (1 << (4)) + (1 << (7));
-				break;
-			case 'c':
-				starting ^= (1 << (0)) + (1 << (3)) + (1 << (6));
-				break;
-		}
+		starting = flipRowColumn(p, starting);
 		std::cout << "\n\nCURRENT: " << std::endl;
-		showTable(starting);	
+		showTable(starting);
 		std::cout << "\n\nTARGET: " << std::endl;
 		showTable(target);
 	}
 	return 0;
+}
+
+int flipRowColumn(char p, int s)
+{
+	switch (p) {
+	case '0':
+		return s ^ (1 << (8)) + (1 << (7)) + (1 << (6));
+	case '1':
+		return s ^ (1 << (5)) + (1 << (4)) + (1 << (3));
+	case '2':
+		return s ^ (1 << (2)) + (1 << (1)) + (1 << (0));
+	case 'a':
+		return s ^ (1 << (2)) + (1 << (5)) + (1 << (8));
+	case 'b':
+		return s ^ (1 << (1)) + (1 << (4)) + (1 << (7));
+	case 'c':
+		return s ^ (1 << (0)) + (1 << (3)) + (1 << (6));
+	}
+	return s;
 }
 
 void showTable(int n) {
