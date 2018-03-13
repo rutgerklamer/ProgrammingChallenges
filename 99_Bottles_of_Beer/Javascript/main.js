acd94f9a7dce748da9e6a94bab23443c60e30344
Your Name <you@example.com>
2018-03-13 09:56:33 +0100

    Add 99 bottles of beer javascript

---
+++ 99_Bottles_of_Beer/Javascript/main.js
@@ -0,0 +1,3 @@
+for (let i = 99; i > 0; i--) 
+	document.write(i, " Bottles of beer on the wall, ", i,  " Bottles of beer.<br>Take one down and pass it around, ", i-1, " Bottles of beer on the wall<br><br>")
+document.write("No more Bottles of beer on the wall, ",  "No more Bottles of beer.<br>Go to the store and buy some more, ", "99 Bottles of beer on the wall<br>")
