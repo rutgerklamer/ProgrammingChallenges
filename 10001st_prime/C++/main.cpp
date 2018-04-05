0c32f78ea781a0b96103c43603b80cf41f28407e
rutgerklamer <rutger.klamer@gmail.com>
2018-04-05 13:02:06 +0200

    Add 10001st prime

---
+++ 10001st_prime/C++/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+bool is_prime(unsigned long long n);
+
+int main() {
+	int primes = 0;
+	for (unsigned long long j = 0; j < 18446744073709551615; j++) {
+		if (is_prime(j))
+			primes++;
+		if (primes >= 10001) {
+			std::cout << j << std::flush;
+			return 0;
+		}
+	}
+	return 0;
+}
+
+bool is_prime(unsigned long long n)
+{
+	if (n < 2) return false;
+	if (n == 2) return true;
+	if (n % 2 == 0) return false;
+	for (int i = 3; (i*i) <= n; i += 2){
+		if (n % i == 0) return false;
+	}
+	return true;
+}
