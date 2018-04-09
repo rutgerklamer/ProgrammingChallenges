d7446beb87bc51310fe0efa2b9e7a71e550a0072
rutgerklamer <rutger.klamer@gmail.com>
2018-04-09 15:59:44 +0200

    Fix style

--- 10001st_prime/C++/main.cpp
+++ 10001st_prime/C++/main.cpp
@@ -20,8 +20,8 @@ bool is_prime(unsigned long long n)
 	if (n < 2) return false;
 	if (n == 2) return true;
 	if (n % 2 == 0) return false;
-	for (int i = 3; (i*i) <= n; i += 2){
-		if (n % i == 0) return false;
-	}
+	for (int i = 3; (i*i) <= n; i += 2)
+		if (n % i == 0) 
+			return false;
 	return true;
 }
