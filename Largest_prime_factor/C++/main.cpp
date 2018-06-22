a2de4bc1777ddeb2059cf023cf0e873f4385fe0f
Rutger Klamer <rutger@rutgerklamer.nl>
2018-06-22 12:30:59 +0200

    Made the program smaller

--- Largest_prime_factor/C++/main.cpp
+++ Largest_prime_factor/C++/main.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 
 int main() {
-	unsigned long long number = 600851475143;
-	bool is_prime = true;
+	long number = 600851475143;
 	for (int i = 2; i < number; i++) {
-		is_prime = true;
+		bool is_prime = true;
 		for (int j = 2; j < i; j++) 
 			if (i % j == 0)
 				is_prime = false;
