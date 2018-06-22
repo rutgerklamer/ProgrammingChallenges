d0110292effa311630ff3249c0bdde97d1d1ce69
Rutger Klamer <rutger@rutgerklamer.nl>
2018-06-22 12:44:27 +0200

    Made program smaller

--- Largest_Palindrome_Number/C++/main.cpp
+++ Largest_Palindrome_Number/C++/main.cpp
@@ -1,32 +1,25 @@
 #include <iostream>
 
-bool is_palindrome(int n);
+void is_largest_palindrome(int n, int &largest);
 
 int main()
 {
 	int largets_palindrome = 0;
-	for (int i = 100; i < 999; i++) {
-		for (int j = 100; j < 999; j++) {
-			int a = i*j;
-			if (is_palindrome(a) && a > largets_palindrome) 
-					largets_palindrome = a;
-		}
-	}
+	for (int i = 100; i < 999; i++) 
+		for (int j = 100; j < 999; j++) 
+			is_largest_palindrome(i*j, largets_palindrome);
 	std::cout << largets_palindrome << std::endl;
 	return 0;
 }
 
-bool is_palindrome(int n)
+void is_largest_palindrome(int n, int &largest)
 {
-	int l = 0;
-	for (int i = n; i != 0; i /= 10)
-		l++;
-	int da = 1;
-	int a = 0;
-	for (int i = 1; i <= l; i++)
+	int nm = n, a = 0;
+	while(nm > 0)
 	{
-		a += ((n / da) % 10) * (pow(10, l - i));
-		da = da * 10;
+		a = a*10 + (nm % 10);
+		nm /= 10;
 	}
-	return (a == n ? true : false);
+	if (n > largest && a==n)
+		largest = n;
 }
