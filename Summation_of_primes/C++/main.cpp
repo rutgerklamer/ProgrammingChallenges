2f4559e225ea46512ad11958eb355aac83e854ff
Rutger Klamer <rutger@rutgerklamer.nl>
2018-06-22 13:04:13 +0200

    displaying sum + data type improvements

--- Summation_of_primes/C++/main.cpp
+++ Summation_of_primes/C++/main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
-bool is_prime(unsigned long long n);
+bool is_prime(int n);
 
 int main() {
-	unsigned long long sum = 0;
+	long sum = 0;
 	for (int i = 2; i < 2000000; i++)
 		if (is_prime(i))
 			sum += i;
+	std::cout << sum << std::endl;
 	return 0;
 }
 
-bool is_prime(unsigned long long n)
+bool is_prime(int n)
 {
 	if (n == 2) return true;
 	if (n % 2 == 0) return false;
