fb073e480893b8773d30f228f462b13966b07e0c
Rutger Klamer <rutger@rutgerklamer.nl>
2018-06-22 12:57:53 +0200

    Add a smaller improved code

--- 10001st_prime/C++/main.cpp
+++ 10001st_prime/C++/main.cpp
@@ -1,25 +1,21 @@
 #include <iostream>
 
-bool is_prime(unsigned long long n);
+bool is_prime(int n);
 
 int main() {
-	int primes = 0;
-	for (unsigned long long j = 0; j < 18446744073709551615; j++) {
-		if (is_prime(j))
+	int primes, j = primes = 0;
+	while(primes < 10001) {
+		if (is_prime(j++))
 			primes++;
-		if (primes >= 10001) {
-			std::cout << j << std::flush;
-			return 0;
-		}
 	}
+	std::cout << j -1 << std::flush;
 	return 0;
 }
 
-bool is_prime(unsigned long long n)
+bool is_prime(int n)
 {
-	if (n < 2) return false;
 	if (n == 2) return true;
-	if (n % 2 == 0) return false;
+	if (n % 2 == 0 || n < 2) return false;
 	for (int i = 3; (i*i) <= n; i += 2)
 		if (n % i == 0) 
 			return false;
